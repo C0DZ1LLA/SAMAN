dc8aa6eea768374d5d08b4cb9fcec09e3f94bc9d
C0DZ1LLA <122047403+C0DZ1LLA@users.noreply.github.com>
2024-08-01 20:35:23 +0300

    UPDATE

--- C0DZ1LLA/SHOPTEST/script.js
+++ C0DZ1LLA/SHOPTEST/script.js
@@ -2,7 +2,7 @@
 const stripe = Stripe('pk_test_51HbuSeAhcFh6mP3tk2aJDLoRC5AFkwFLk4ZxtVCGYQAzjAWsFGTwRetAaH2sWcSLJsUvKTjQPLllpwJWDfxaIbwr00Kf3IXT3B');
 
 document.getElementById('checkout-button').addEventListener('click', () => {
-    fetch('/create-checkout-session', {
+    fetch('https://c0dz1lla.glitch.me/create-checkout-session', {  // Update this line
         method: 'POST',
     })
     .then(response => response.json())
