5d89a54915e6a36721859a4febcf50244ad46e55
C0DZ1LLA <122047403+C0DZ1LLA@users.noreply.github.com>
2024-08-01 19:40:49 +0300

    PUSH

---
+++ C0DZ1LLA/SHOPTEST/script.js
@@ -0,0 +1,20 @@
+// Stripe public key
+const stripe = Stripe('pk_test_51HbuSeAhcFh6mP3tk2aJDLoRC5AFkwFLk4ZxtVCGYQAzjAWsFGTwRetAaH2sWcSLJsUvKTjQPLllpwJWDfxaIbwr00Kf3IXT3B');
+
+document.getElementById('checkout-button').addEventListener('click', () => {
+    fetch('/create-checkout-session', {
+        method: 'POST',
+    })
+    .then(response => response.json())
+    .then(session => {
+        return stripe.redirectToCheckout({ sessionId: session.id });
+    })
+    .then(result => {
+        if (result.error) {
+            alert(result.error.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
